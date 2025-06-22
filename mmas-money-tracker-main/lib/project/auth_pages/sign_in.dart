1ec98aa6f129c9eb2497b52df55c7eee433d5f7a
AH_NQ <tuananh030305@gmail.com>
2025-06-23 00:18:03 +0700

    Update sign_in.dart

--- mmas-money-tracker-main/lib/project/auth_pages/sign_in.dart
+++ mmas-money-tracker-main/lib/project/auth_pages/sign_in.dart
@@ -14,7 +14,7 @@ class SignIn extends StatelessWidget {
   Future<String?>? _onRecoverPassword(String name) {
     print('onRecoverPassword');
     return Future.delayed(Duration(seconds: 1)).then((_) => null);
-  }
+  } 
 
   @override
   Widget build(BuildContext context) {
